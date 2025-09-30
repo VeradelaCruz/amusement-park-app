98c7ed313a07c8a420923af1d0529ab52c1b1aae
VeradelaCruz <maidanabru@yahoo.com.ar>
2025-09-30 17:59:46 +0200

    Fix: buyer, games, and tickets service pom file

--- employees-service/src/main/java/com/example/employees_service/EmployeesServiceApplication.java
+++ employees-service/src/main/java/com/example/employees_service/EmployeesServiceApplication.java
@@ -2,8 +2,12 @@ package com.example.employees_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.example.employees_service.client")
 public class EmployeesServiceApplication {
 
 	public static void main(String[] args) {
