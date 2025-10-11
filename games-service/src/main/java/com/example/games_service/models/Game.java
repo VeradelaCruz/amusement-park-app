e1e04de3a2c1beca664e01ddd835628de9088b46
VeradelaCruz <maidanabru@yahoo.com.ar>
2025-10-11 06:56:19 +0200

    Fix: validation of atributes in games-service

--- games-service/src/main/java/com/example/games_service/models/Game.java
+++ games-service/src/main/java/com/example/games_service/models/Game.java
@@ -1,23 +1,36 @@
 package com.example.games_service.models;
 
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.*;
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalTime;
 import java.util.List;
-
 @Data
 @Document(collection = "games")
 public class Game {
+
     @Id
     private String gameId;
-    @NotEmpty(message = "Name can not be empty")
+
+    @NotEmpty(message = "Name cannot be empty")
+    @Size(max = 50, message = "Game name cannot exceed 50 characters")
     private String gameName;
+
+    @Min(value = 1, message = "Duration must be at least 1 minute")
+    @Max(value = 30, message = "Duration cannot exceed 30 minutes")
     private int duration;
+
+    @NotNull(message = "Start time cannot be null")
     private LocalTime startTime;
+
+    @NotNull(message = "End time cannot be null")
     private LocalTime endTime;
+
+    @NotNull(message = "Price cannot be null")
+    @Positive(message = "Price must be greater than zero")
     private Double priceGame;
 
 
