1ab507ae22eb71c75ca79e2d0a44c0a88a10a548
VeradelaCruz <maidanabru@yahoo.com.ar>
2025-10-24 06:39:30 +0200

    Add: KafkaProducerConfiguration for ticket-service

--- tickets-service/src/main/java/com/example/tickets_service/service/kafka/TicketConsumer.java
+++ tickets-service/src/main/java/com/example/tickets_service/service/kafka/TicketConsumer.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TicketConsumer {
-    
+
     @KafkaListener(topics = "tickets", groupId = "ticket-service-group")
     public void listenBuyerEvent(String message) {
         System.out.println("Received event in tickets-service: " + message);
