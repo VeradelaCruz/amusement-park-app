0cdb9fdbcba429db0c6aab5f668bc5e2247af69a
VeradelaCruz <maidanabru@yahoo.com.ar>
2025-10-24 06:36:42 +0200

    Add: ticket consumer for ticket-service

--- tickets-service/src/main/java/com/example/tickets_service/service/kafka/TicketConsumer.java
+++ tickets-service/src/main/java/com/example/tickets_service/service/kafka/TicketConsumer.java
@@ -1,4 +1,13 @@
 package com.example.tickets_service.service.kafka;
 
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TicketConsumer {
+    
+    @KafkaListener(topics = "tickets", groupId = "ticket-service-group")
+    public void listenBuyerEvent(String message) {
+        System.out.println("Received event in tickets-service: " + message);
+    }
 }
